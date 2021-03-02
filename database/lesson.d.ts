0103d408927227c794069660812b2d70401763a8
James Burdge <jamesedburdge@gmail.com>
2021-03-02 18:45:50 +0000

    initial commit

---
+++ database/lesson.d.ts
@@ -0,0 +1,7 @@
+declare namespace Morasun {
+  namespace DB {
+    type lesson = {
+      lesson_id:number
+    }
+  }
+}
---
+++ index.d.ts
@@ -0,0 +1,5 @@
+declare namespace Morasun {
+  namespace DB {
+    
+  }
+}
